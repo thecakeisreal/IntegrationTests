4c0217513c929a2d5da921223808d3ed64e01fc9
thecakeisreal <36107760+thecakeisreal@users.noreply.github.com>
2021-10-26 10:34:18 -0400

    Update Program.cs

--- IntegrationTests/Program.cs
+++ IntegrationTests/Program.cs
@@ -6,7 +6,7 @@ namespace IntegrationTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Le PGCD de 6 et 15 est { MathExt.PGCD(6, 15) }");
+            //Console.WriteLine($"Le PGCD de 6 et 15 est { MathExt.PGCD(6, 15) }");
             Console.WriteLine($"Le PPCM de 6 et 15 est { MathExt.PPCM(6, 15) }");
         }
     }
