7362ffee51c7c88891aad627ffd95ad1153a3ca6
thecakeisreal <36107760+thecakeisreal@users.noreply.github.com>
2021-10-26 10:39:02 -0400

    Renommage

---
+++ Tests/MathExtTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class MathExtTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
